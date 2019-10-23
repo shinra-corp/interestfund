34b0bb985995654fd368685283576bb85b46c569
Axe <ngsmo@iscte-iul.pt>
2019-10-23 18:20:39 +0100

    Fund deploy test

--- test/FundFactoryTest.js
+++ test/FundFactoryTest.js
@@ -2,6 +2,7 @@ const ENS = require("../build/ENSMock");
 
 const etherlime = require('etherlime-lib');
 const FundFactory = require("../build/FundFactory.json");
+const Fund = require("../build/Fund.json");
 const Dispatcher = require("../build/Dispatcher.json");
 const TokenResolver = require("../build/Resolver.json");
 const DomainController = require("../build/DomainController.json");
@@ -9,7 +10,6 @@ const utils = require("./utils/utils.js");
 const rootNode = utils.namehash("interestfund.eth");
 
 describe('Fund Factory Contract Test', () => {
-
 
     let Owner = accounts[0];
     let deployer;
@@ -46,16 +46,28 @@ describe('Fund Factory Contract Test', () => {
 
     });
 
-    it('should have a valid owner', async () => {
+    it('should have a valid configuration', async () => {
         let _owner = await factory.owner();
         assert.strictEqual(Owner.signer.address, _owner);
+        let _controller = await factory.controller();
+        assert.strictEqual(controller.contractAddress, _controller);
     });
 
     it('should create a new Fund', async() => {
         let tx = await factory.newFunding("Funding");
         let result = await factory.verboseWaitForTransaction(tx, 'creating new fund');
+        //console.log(result.events[2].args.at);
         assert.strictEqual(result.events[2].event, "NewFunding");
+
+        let deployFund = await etherlime.ContractAt(Fund, result.events[2].args.at)
+
+        let manager = await deployFund.manager();
+        assert.strictEqual(manager, Owner.signer.address);
     });
 
+    it('should emit Domain Change', async () => {
+        let tx = await factory.setDomainController(Owner.signer.address);
+        let result = await factory.verboseWaitForTransaction(tx, 'change Domain controller');
+        assert.strictEqual(result.events[0].event, 'DomainControllerChange');
+    });
 });
-
