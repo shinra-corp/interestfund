43320055a3022fdf9d7b30e689903d7c7aca62a9
Axe <ngsmo@iscte-iul.pt>
2019-11-01 21:34:19 +0000

    deployer to ganache network

---
+++ config.js
@@ -0,0 +1,25 @@
+module.exports = {
+
+    DAI: {
+        Mainnet : "0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359",
+        Kovan : "0xC4375B7De8af5a38a93548eb8453a498222C4fF2"
+    },
+
+    Compound: {
+        cDAI {
+            Mainnet: "0xf5dce57282a584d2746faf1593d3121fcac444dc",
+            Rinkeby: "0x6d7f0754ffeb405d23c51ce938289d4835be3b14",
+            Kovan: "0x0a1e4d0b5c71b955c0a5993023fc48ba6e380496",
+            Ropsten: "0x2b536482a01e620ee111747f8334b395a42a555e",
+            Goerli: "0xd9fd9e875c9c1d567825e431dd6ed4f0e51aa8bf"
+        }
+    }
+
+    ENS: {
+        Mainnet: "0x314159265dd8dbb310642f98f50c066173c1259b",
+        Rinkeby: "0xe7410170f87102df0055eb195163a03b7f2bff4a",
+        Ropsten: "0x112234455c3a32fd11230c42e7bccd4a84e02010",
+        Goerli: "0x112234455c3a32fd11230c42e7bccd4a84e02010"
+    }
+
+}
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,64 @@
+const utils = require("../test/utils/utils.js");
+//const endpoints = require("../config.js");
+const rootNode = utils.namehash("interestfund.eth");
+
+const FundFactory = artifacts.require("FundFactory");
+const Fund = artifacts.require("Fund");
+const DomainController = artifacts.require("DomainController");
+
+const Resolver = artifacts.require("ENSResolverMock");
+const ENS = artifacts.require("ENSMock");
+const DAI = artifacts.require("ERC20Mock");
+const Compound = artifacts.require("CErc20");
+
+module.exports = function(deployer, network, accounts) {
+
+    switch(network) {
+        case "Mainnet": _deployToMainnet(deployer, accounts); break;
+        case "Ropsten": _deployToRopsten(deployer, accounts); break;
+        case "Goerli": _deployToGoerli(deployer, accounts); break;
+        default: _deployToGanache(deployer, accounts[0]); break;
+    }
+}
+
+
+function _deployToMainnet(deployer, accounts) {
+    return;
+}
+
+function _deployToRopsten(deployer, accounts) {
+    return;
+}
+
+function _deployToGoerli(deployer, accounts) {
+    return;
+}
+
+//If deployment to private network than deploy also mocks contracts
+function _deployToGanache(deployer, owner) {
+
+    let _dai, _ctoken, _resolver, _factory, _ensMock, _controller;
+
+    console.log("Starting Ganache Deployment");
+
+    let supplyRate = "2000000000";
+
+    deployer.deploy(Resolver).then(function(instance) {
+        _resolver = instance;
+    });
+
+    deployer.deploy(DAI, owner, "10000000000000000000").then(function(instance) {
+        _dai = instance;
+        return deployer.deploy(Compound, instance.address, supplyRate);
+    }).then(function(instance) {
+        _ctoken = instance;
+        return deployer.deploy(FundFactory, _dai.address, instance.address);
+    }).then(function(instance) {
+        _factory = instance;
+        return deployer.deploy(ENS, rootNode, owner);
+    }).then(function(instance) {
+        _ensMock = instance;
+        return deployer.deploy(DomainController, rootNode, _factory.address, _ensMock.address, _resolver.address);
+    });
+
+}
