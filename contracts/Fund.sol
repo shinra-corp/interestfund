6708822a013c62086721e3a0d098a7a7d6fbabfe
Axe <ngsmo@iscte-iul.pt>
2019-11-12 17:35:08 +0000

    comment fund public/external functions

--- contracts/Fund.sol
+++ contracts/Fund.sol
@@ -5,6 +5,9 @@ import "./utils/ReentrancyGuard.sol";
 import "./interfaces/IERC20.sol";
 import "./interfaces/ICToken.sol";
 
+/// @title Fund Wallet
+/// @notice You should interact with this contract to start/end funding this project. Each project have a Fund contract.
+/// @dev A new Fund contract is instanciated from Factory Pattern. See FundFactory.sol.
 contract Fund is ReentrancyGuard {
 
     using SafeMath for uint256;
@@ -38,7 +41,9 @@ contract Fund is ReentrancyGuard {
         compoundToken = ICToken(_compoundToken);
     }
 
-
+    ///@notice Start funding this project with some amount of DAI.
+    ///@dev This function should only be call after the user approved DAI with this contract address.
+    ///@param _amount The amount of DAI token to send to this project wallet.
     function funding(uint256 _amount) public nonReentrant {
 
         require(_amount != 0, "Error: define amount");
@@ -62,6 +67,9 @@ contract Fund is ReentrancyGuard {
     }
 
 
+    ///@notice Get back a previous send amount of DAI token.
+    ///@dev This function after validating withdraw amounts will call 'transfer' on DAI contract with the caller address.
+    ///@param _amount The amount of DAI token to withdraw from this project wallet. Use 18 decimals
     function withdraw(uint256 _amount) public nonReentrant {
 
         require(userBalances[msg.sender] >= _amount, 'Error: not enough balance');
@@ -79,7 +87,9 @@ contract Fund is ReentrancyGuard {
         emit Withdraw(msg.sender, _amount);
     }
 
-
+    ///@notice Get fund generated interest in DAI. Can't withdraw users balances.
+    ///@dev This function after validating withdraw amounts will call 'transfer' on DAI contract with the caller address.
+    ///@param _amount The amount of DAI token to withdraw from this project wallet. Use 18 decimals
     function withdrawInterest(uint256 _amount) public onlyManager nonReentrant {
         //get compound balance
         uint cbalance = compoundToken.balanceOf(address(this));
@@ -90,12 +100,15 @@ contract Fund is ReentrancyGuard {
         emit Withdraw(msg.sender, _amount);
     }
 
+    ///@notice Return the amount of accrued interest from fund on Compound market.
+    ///@return _amount The amount of interest generated. Use 18 decimals
     function accruedInterest() public view returns(uint256) {
         uint256 cbalance = compoundToken.balanceOfUnderlying(address(this));
         return cbalance.sub(totalBalances);
     }
 
-
+    ///@notice Return the balance of a user in this contract.
+    ///@return _amount The amount of donation given by user. Use 18 decimals
     function balanceOf(address _donor) public view returns(uint256) {
         return userBalances[_donor];
     }
