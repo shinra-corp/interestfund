66652b947c3b565cd4691363697775cf86b1e6a1
Axe <ngsmo@iscte-iul.pt>
2019-11-13 18:03:56 +0000

    Added comments

--- contracts/FundFactory.sol
+++ contracts/FundFactory.sol
@@ -10,6 +10,8 @@ interface IDomainController {
     function changeAsker(address _newAsker) external;
 }
 
+/// @title Fund Factory Wallet
+/// @notice You should interact with this contract to create a new Fund instance. Each Fund has a unique URI that will have a ENS subdomain.
 contract FundFactory is Ownable {
 
     event NewFunding(address indexed _manager, address indexed _at);
@@ -32,7 +34,9 @@ contract FundFactory is Ownable {
         compoundToken = _compoundToken;
     }
 
-
+    ///@notice Create a new Fund project with a subdomain. The new Fund will have endpoint configured for you.
+    ///@notice The new Fund address is saved to facilite frontend listing
+    ///@param _URI Define the name of the new Fund. Must be unique.
     function newFunding(string memory _URI) public {
         Fund _fund = new Fund(msg.sender, daiToken, compoundToken);
         funds.push(address(_fund));
@@ -44,20 +48,24 @@ contract FundFactory is Ownable {
     }
 
 
+    ///@notice Define a new endpoint of the smart contract responsable to manage the issuing of new subdomains.
+    ///@param _controller New Domain Controller address
     function setDomainController(address _controller) public onlyOwner {
         require(_controller != address(0), 'Error: Controller invalid');
 
         emit DomainControllerChange(address(controller), _controller);
         controller = IDomainController(_controller);
     }
-
+    ///@notice Define a new DAI contract used by Fund instances.
+    ///@param _newDaiToken New DAI contract address
     function setDAIToken(address _newDaiToken) public onlyOwner {
         require(_newDaiToken != address(0), 'Error: DAI Address invalid');
 
         emit DAITokenChange(daiToken, _newDaiToken);
         daiToken = _newDaiToken;
     }
-
+    ///@notice Define a new Compound market contract used by Fund instances.
+    ///@param _newCompoundToken New Compound Market address
     function setCompoundToken(address _newCompoundToken) public onlyOwner {
         require(_newCompoundToken != address(0), 'Error: Compound Token invalid');
 
