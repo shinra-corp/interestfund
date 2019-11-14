c0e46b7e673930585db0b5bee8a83c1dc5e8632a
Axe <ngsmo@iscte-iul.pt>
2019-11-14 11:18:57 +0000

    comments

--- contracts/utils/Ownable.sol
+++ contracts/utils/Ownable.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.5.11;
 
+/// @title Ownable Contract functions
+/// @notice This contract allow for defining the owner of the child contract. To change ownership the new Owner must acceptOwnership.
 contract Ownable {
 
     address public owner;
@@ -10,15 +12,18 @@ contract Ownable {
 
     constructor() internal {
         owner = msg.sender;
-
     }
 
+    ///@notice Change ownership of contract
+    ///@dev The new owner must accept the ownership
+    ///@param _newOwner address of the new Owner
     function changeOwner(address _newOwner) public onlyOwner {
         require(_newOwner != address(0), 'Must be a valid new Owner');
         nextOwner = _newOwner;
         emit ProposedNewOwner(_newOwner);
     }
 
+    ///@notice Accept ownership of contract
     function acceptOwnership() public {
         require(msg.sender == nextOwner, 'not the next owner');
         owner = nextOwner;
